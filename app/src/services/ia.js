966dd09c4d0e3beca8881943e8f1df7952d4302d
Igor Amarilha <120053865+Amarilha@users.noreply.github.com>
2025-05-21 09:07:45 -0300

    key

--- app/src/services/ia.js
+++ app/src/services/ia.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const GEMINI_API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KE;
+const GEMINI_API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
 let chatHistory = [];
